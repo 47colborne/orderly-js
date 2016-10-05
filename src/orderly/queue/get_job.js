7f0e94a212ec6e8d4e02d95d0f39b26f2e153dfe
scottchu <scott@retailcommon.com>
2016-10-05 10:22:52 -0400

    remove log from get_job temporarily

--- src/orderly/queue/get_job.js
+++ src/orderly/queue/get_job.js
@@ -1,10 +1,3 @@
-import { log } from '../debug'
-
-function logAction(queue) {
-  log('Queue', 'getting a job', { size: queue.q.size })
-}
-
 export function getJob(queue) {
-  logAction(queue)
   return queue.q.poll()
 }
